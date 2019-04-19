755dc396baaff1de901edbff80610bba910f7329
canadmin <icanyard@gmail.com>
2019-04-20 02:27:47 +0300

    Pojo eklendi.

---
+++ src/main/java/can/springframework/petclinic/model/PetType.java
@@ -0,0 +1,14 @@
+package can.springframework.petclinic.model;
+
+public class PetType {
+
+    private String petType;
+
+    public String getPetType() {
+        return petType;
+    }
+
+    public void setPetType(String petType) {
+        this.petType = petType;
+    }
+}
---
+++ src/main/java/can/springframework/petclinic/model/Vet.java
@@ -0,0 +1,6 @@
+package can.springframework.petclinic.model;
+
+public class Vet extends Person {
+
+
+}
