e66e3cf72025ef4e9fd73b5316e799496dbc7863
canadmin <icanyard@gmail.com>
2019-04-24 02:14:44 +0300

    impl map base service.

---
+++ src/main/java/can/springframework/petclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,33 @@
+package can.springframework.petclinic.services.map;
+
+import can.springframework.petclinic.model.Owner;
+import can.springframework.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(),object);
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+}
