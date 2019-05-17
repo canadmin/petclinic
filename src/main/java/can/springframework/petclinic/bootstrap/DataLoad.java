f3dd28e9769c3ac1975e89b8ba6747df9151ef16
canadmin <icanyard@gmail.com>
2019-05-17 22:05:32 +0300

    Refactor Map  ımpl to abstract ID

--- src/main/java/can/springframework/petclinic/bootstrap/DataLoad.java
+++ src/main/java/can/springframework/petclinic/bootstrap/DataLoad.java
@@ -1,52 +1,1 @@
-package can.springframework.petclinic.bootstrap;
-
-import can.springframework.petclinic.model.Owner;
-import can.springframework.petclinic.model.Vet;
-import can.springframework.petclinic.services.OwnerService;
-import can.springframework.petclinic.services.VetService;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
-
-@Component
-public class DataLoad implements CommandLineRunner {
-    private final OwnerService ownerService;
-    private final VetService vetService;
-
-    public DataLoad(OwnerService ownerService, VetService vetService) {
-        this.ownerService = ownerService;
-        this.vetService = vetService;
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-
-        Owner owner1=new Owner();
-        owner1.setId(1L);
-        owner1.setFirstName("Can");
-        owner1.setLastName("Yard");
-
-        ownerService.save(owner1);
-
-        Owner owner2=new Owner();
-        owner2.setId(2L);
-        owner2.setFirstName("Mustafa");
-        owner2.setLastName("coskun");
-
-        ownerService.save(owner2);
-
-        Vet vet1=new Vet();
-        vet1.setId(1L);
-        vet1.setFirstName("Sam");
-        vet1.setLastName("Axe");
-        vetService.save(vet1);
-
-        Vet vet2=new Vet();
-        vet2.setId(2L);
-        vet2.setFirstName("Can");
-        vet2.setLastName("yard");
-
-        vetService.save(vet2);
-
-
-    }
-}
+package can.springframework.petclinic.bootstrap;import can.springframework.petclinic.model.Owner;import can.springframework.petclinic.model.Vet;import can.springframework.petclinic.services.OwnerService;import can.springframework.petclinic.services.VetService;import org.springframework.boot.CommandLineRunner;import org.springframework.stereotype.Component;@Componentpublic class DataLoad implements CommandLineRunner {    private final OwnerService ownerService;    private final VetService vetService;    public DataLoad(OwnerService ownerService, VetService vetService) {        this.ownerService = ownerService;        this.vetService = vetService;    }    @Override    public void run(String... args) throws Exception {        Owner owner1=new Owner();        owner1.setFirstName("Can");        owner1.setLastName("Yard");        ownerService.save(owner1);        Owner owner2=new Owner();        owner2.setFirstName("Mustafa");        owner2.setLastName("coskun");        ownerService.save(owner2);        Vet vet1=new Vet();        vet1.setFirstName("Sam");        vet1.setLastName("Axe");        vetService.save(vet1);        Vet vet2=new Vet();        vet2.setFirstName("Can");        vet2.setLastName("yard");        vetService.save(vet2);    }}
--- src/main/java/can/springframework/petclinic/services/map/AbstractMapService.java
+++ src/main/java/can/springframework/petclinic/services/map/AbstractMapService.java
@@ -1,13 +1,13 @@
 package can.springframework.petclinic.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import can.springframework.petclinic.model.BaseEntity;
 
-public abstract class AbstractMapService<T, ID> {
+import java.util.*;
 
-    protected Map<ID, T> map = new HashMap<>();
+
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+
+    protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll() {
         return new HashSet<>(map.values());
@@ -17,8 +17,15 @@ public abstract class AbstractMapService<T, ID> {
         return map.get(id);
     }
 
-    T save(ID id, T object) {
-        map.put(id, object);
+    T save(T object) {
+        if(object != null){ // nesne boş değil
+            if(object.getId() == null){ // ve id verilmemiş ise en büyük id nin bir fazlasını veriyoruz
+                object.setId(getNextId());
+            }
+            map.put(object.getId(),object);
+        }else{
+            throw  new RuntimeException("Object cannot be null");
+        }
         return object;
     }
 
@@ -27,5 +34,15 @@ public abstract class AbstractMapService<T, ID> {
     }
     void delete(T object){
         map.entrySet().removeIf(entry->entry.getValue().equals(object));
+    }
+    private Long getNextId(){ // otomatik ıd verme özelliğini aktifleştiriyoruz
+        Long nextId=null;
+        try{
+            nextId=Collections.max(map.keySet())+1;
+        }catch (Exception e){
+            nextId=1L;
+        }
+
+        return nextId;
     }
 }
